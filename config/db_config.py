2d01493034abe1125bfb9179f8c84dd83d815ed7
no-acct-journal <arthurshen168@gmail.com>
2026-08-20 23:42:58 -0700

    get categories function

--- config/db_config.py
+++ config/db_config.py
@@ -6,28 +6,30 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
-# PostgreSQL 异步数据库 URL
 ASYNC_DATABASE_URL = (
-    "postgresql+asyncpg://postgres:1234@localhost:5432/news_app"
+    "postgresql+asyncpg://postgres:1234@localhost:5432/postgres"
 )
 
-# 创建异步引擎
 async_engine = create_async_engine(
     ASYNC_DATABASE_URL,
-    echo=True,          # 输出 SQL 日志
-    pool_size=10,       # 连接池中保持的持久连接数
-    max_overflow=20,    # 允许创建的额外连接数
-    pool_pre_ping=True  # 使用连接前检查连接是否有效
+    echo=True,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    connect_args={
+        "server_settings": {
+            "search_path": '"news-set"'
+        }
+    },
 )
 
-# 创建异步会话工厂
 AsyncSessionLocal = async_sessionmaker(
     bind=async_engine,
     class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
-# FastAPI 依赖项：获取数据库会话
+
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         try:
--- routers/news.py
+++ routers/news.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config.db_config import get_db
+from service import news
 
 router = APIRouter(prefix="/api/news", tags=["news"])
 
 @router.get("/categories")
-async def get_categories():
-    return {"message": "categories retrieved successfully"}
+async def get_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    categories = await news.get_category(db, skip, limit)
+    return {
+        "code" : 200,
+        "message": "success",
+        "data": categories
+    }
